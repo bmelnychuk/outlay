6fd35f3e6b0d072a43d9637fb0b2e3d3f4cd4d43
Bogdan Melnychuk <melnychuk.bogdan@gmail.com>
2017-02-24 14:08:42 +0100

    User component recreation fix

--- outlay/app/src/main/java/app/outlay/App.java
+++ outlay/app/src/main/java/app/outlay/App.java
@@ -2,7 +2,10 @@ package app.outlay;
 
 import android.app.Application;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
+
 import app.outlay.core.logger.LoggerFactory;
 import app.outlay.di.component.AppComponent;
 import app.outlay.di.component.DaggerAppComponent;
@@ -11,6 +14,7 @@ import app.outlay.di.module.AppModule;
 import app.outlay.di.module.FirebaseModule;
 import app.outlay.di.module.UserModule;
 import app.outlay.domain.model.User;
+import app.outlay.firebase.dto.adapter.UserAdapter;
 import app.outlay.impl.AndroidLogger;
 
 /**
@@ -30,9 +34,7 @@ public class App extends Application {
     }
 
     public UserComponent createUserComponent(User user) {
-        if (userComponent == null) {
-            userComponent = appComponent.plus(new UserModule(user));
-        }
+        userComponent = appComponent.plus(new UserModule(user));
         return userComponent;
     }
 
@@ -52,6 +54,17 @@ public class App extends Application {
     }
 
     public UserComponent getUserComponent() {
+        return getUserComponent(true);
+    }
+
+    public UserComponent getUserComponent(boolean tryRecreate) {
+        if (userComponent == null && tryRecreate) {
+            FirebaseUser fbUser = FirebaseAuth.getInstance().getCurrentUser();
+            if (fbUser != null) {
+                User user = UserAdapter.fromFirebaseUser(fbUser);
+                createUserComponent(user);
+            }
+        }
         return userComponent;
     }
 }
