d191d03cdef46cb0fb76647de512d5de4a0f8f1f
zsmb13 <braunmarci@gmail.com>
2017-04-18 19:02:06 +0200

    Refactored large anonymous inner class, formatted code (fixes #14)

--- outlay/app/src/main/java/app/outlay/view/autocomplete/CategoryAutoCompleteAdapter.java
+++ outlay/app/src/main/java/app/outlay/view/autocomplete/CategoryAutoCompleteAdapter.java
@@ -8,11 +8,9 @@ import android.widget.BaseAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.TextView;
-
-import com.github.johnkil.print.PrintView;
-
 import app.outlay.domain.model.Category;
 import app.outlay.utils.IconUtils;
+import com.github.johnkil.print.PrintView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +19,51 @@ public class CategoryAutoCompleteAdapter extends BaseAdapter implements Filterab
 
     private List<Category> items;
     private List<Category> suggestions;
+
     private LayoutInflater inflater;
+
+    private Filter nameFilter = new NameFilter();
+
+    private class NameFilter extends Filter {
+        @Override
+        public CharSequence convertResultToString(Object resultValue) {
+            return ((Category) resultValue).getTitle();
+        }
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults filterResults = new FilterResults();
+            if (constraint == null) {
+                return filterResults;
+            }
+
+            suggestions.clear();
+
+            String query = constraint.toString().toLowerCase();
+
+            for (Category category : items) {
+                String title = category.getTitle().toLowerCase();
+                if (title.startsWith(query)) {
+                    suggestions.add(category);
+                }
+            }
+
+            filterResults.values = suggestions;
+            filterResults.count = suggestions.size();
+
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            if (results != null && results.count > 0) {
+                notifyDataSetChanged();
+            }
+            else {
+                notifyDataSetInvalidated();
+            }
+        }
+    }
 
     public CategoryAutoCompleteAdapter() {
         super();
@@ -66,7 +108,8 @@ public class CategoryAutoCompleteAdapter extends BaseAdapter implements Filterab
             vh.categoryTitle = (TextView) view.findViewById(app.outlay.R.id.categoryTitle);
 
             view.setTag(vh);
-        } else {
+        }
+        else {
             vh = (ViewHolder) view.getTag();
         }
         vh.categoryTitle.setText(current.getTitle());
@@ -77,46 +120,11 @@ public class CategoryAutoCompleteAdapter extends BaseAdapter implements Filterab
     class ViewHolder {
         TextView categoryTitle;
         PrintView categoryIcon;
+
     }
 
     @Override
     public Filter getFilter() {
         return nameFilter;
     }
-
-    Filter nameFilter = new Filter() {
-        @Override
-        public CharSequence convertResultToString(Object resultValue) {
-            String str = ((Category) resultValue).getTitle();
-            return str;
-        }
-
-        @Override
-        protected FilterResults performFiltering(CharSequence constraint) {
-            FilterResults filterResults = new FilterResults();
-            if (constraint != null) {
-                suggestions.clear();
-                try {
-                    for (int i = 0; i < items.size(); i++) {
-                        if (items.get(i).getTitle().toLowerCase().startsWith(constraint.toString().toLowerCase())) {
-                            suggestions.add(items.get(i));
-                        }
-                    }
-                } catch (Exception e) {
-                }
-                filterResults.values = suggestions;
-                filterResults.count = suggestions.size();
-            }
-            return filterResults;
-        }
-
-        @Override
-        protected void publishResults(CharSequence constraint, FilterResults results) {
-            if (results != null && results.count > 0) {
-                notifyDataSetChanged();
-            } else {
-                notifyDataSetInvalidated();
-            }
-        }
-    };
 }
